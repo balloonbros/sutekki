056e72a5396ce70bfd801a0508312fc9a011ae1a
Shohei Yamasaki <s-yamasaki@pepabo.com>
2015-10-22 16:56:20 +0900

    Testing has_label?

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Idea, type: :model do
-  let!(:published_idea) { Idea.create(title: "hello", body: "world", published: true) }
-  let!(:disabled_idea)  { Idea.create(title: "hello", body: "world", published: false) }
+  let!(:published_idea) { create(:published_idea) }
+  let!(:disabled_idea)  { create(:disabled_idea) }
 
   it "has and belongs to many labels" do
     expect(Idea.reflect_on_association(:labels).macro).to be == :has_and_belongs_to_many
@@ -21,5 +21,19 @@ RSpec.describe Idea, type: :model do
   describe "#disable" do
     it { expect{published_idea.disable}.to change{Idea.published.count}.from(1).to(0) }
     it { expect{published_idea.disable}.to change{Idea.disabled.count}.from(1).to(2) }
+  end
+
+  describe '#has_label?' do
+    let(:idea) { create(:published_idea_with_label) }
+
+    it 'returns true' do
+      label_id = idea.labels.first.id
+      expect(idea.has_label?(label_id)).to be true
+    end
+
+    it 'returns false' do
+      label_id = idea.labels.first.id + 1
+      expect(published_idea.has_label?(label_id)).to be false
+    end
   end
 end
