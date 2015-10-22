25cf754e1e707d49419ef5daacb0a86e3a49ceef
Shohei Yamasaki <s-yamasaki@pepabo.com>
2015-10-22 18:52:58 +0900

    Add association test for Label

--- spec/models/label_spec.rb
+++ spec/models/label_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Label, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has and belongs to many ideas" do
+    expect(Label.reflect_on_association(:ideas).macro).to be == :has_and_belongs_to_many
+  end
 end
