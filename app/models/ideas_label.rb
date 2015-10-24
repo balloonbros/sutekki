2daffa266d46cbc4a4d0e2cd71cd0785ca04d47a
Shohei Yamasaki <s-yamasaki@pepabo.com>
2015-10-24 16:53:30 +0900

    Add uniqueness validation

--- app/models/ideas_label.rb
+++ app/models/ideas_label.rb
@@ -1,4 +1,5 @@
 class IdeasLabel < ActiveRecord::Base
   validates :idea_id, presence: true
   validates :label_id, presence: true
+  validates :idea_id, uniqueness: {scope: :label_id}
 end
--- spec/models/ideas_label_spec.rb
+++ spec/models/ideas_label_spec.rb
@@ -10,4 +10,10 @@ RSpec.describe IdeasLabel, type: :model do
     ideas_label = build(:ideas_label, label_id: "")
     expect(ideas_label).to be_invalid
   end
+
+  it "is invalid not unique" do
+    ideas_label = create(:ideas_label)
+    duplicate = build(:ideas_label, idea_id: ideas_label.idea_id, label_id: ideas_label.label_id)
+    expect(duplicate).to be_invalid
+  end
 end
