8abf8fef6f28c397226c041052945784f27d323f
Shohei Yamasaki <s-yamasaki@pepabo.com>
2015-11-26 19:28:15 +0900

    Add sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'ideas#index'
+  get 'sessions/new'
   get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
   resources :labels
   resources :ideas
