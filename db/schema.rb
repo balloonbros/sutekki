82db9d589481bdb6aefe44cabdb2ad4b86c8f622
Shohei Yamasaki <yamasaki0406@gmail.com>
2016-03-18 21:12:53 +0900

    Generate favorite model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210050418) do
+ActiveRecord::Schema.define(version: 20160318120610) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "idea_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "favorites", ["user_id", "idea_id"], name: "index_favorites_on_user_id_and_idea_id", unique: true
 
   create_table "ideas", force: :cascade do |t|
     t.string   "title"
